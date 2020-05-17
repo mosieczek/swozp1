525263381c1b6139e07c8a3f1609f019f600fa90
mosieczek <ihatebisons@gmail.com>
2020-05-17 18:18:30 +0200

    strona główna - main logowanie rejestracja

---
+++ swozp1/app/src/main/java/com/example/swozp1/Logowanie.java
@@ -0,0 +1,14 @@
+package com.example.swozp1;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Logowanie extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logowanie);
+    }
+}
